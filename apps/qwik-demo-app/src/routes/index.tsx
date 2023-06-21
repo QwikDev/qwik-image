e350cd6e605ecd6a1a89612592e8a97213656622
Dustin Silk <dustin@staze.com>
2023-06-21 13:17:14 +0200

    cleanup tests

--- apps/qwik-demo-app/src/routes/index.tsx
+++ apps/qwik-demo-app/src/routes/index.tsx
@@ -1,36 +1,6 @@
-import { component$, useSignal } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import { providers, selectedProvider } from '../providers';
-import { Image, ImageProps } from 'qwik-image';
-
-const DynamicImage = component$(
-  (props: { before: ImageProps; after: ImageProps }) => {
-    const width = useSignal(props.before.width);
-    const height = useSignal(props.before.height);
-    const layout = useSignal(props.before.layout);
-    const src = useSignal(props.before.src);
-
-    return (
-      <>
-        <Image
-          width={width.value}
-          height={height.value}
-          layout={layout.value}
-          src={src.value}
-        />
-        <button
-          onClick$={() => {
-            width.value = props.after.width;
-            height.value = props.after.height;
-            layout.value = props.after.layout;
-            src.value = props.after.src;
-          }}
-        >
-          trigger
-        </button>
-      </>
-    );
-  }
-);
+import { Image } from 'qwik-image';
 
 export default component$(() => {
   return (
@@ -43,20 +13,6 @@ export default component$(() => {
         placeholder="#e6e6e6"
         layout="constrained"
         src={providers[selectedProvider].src}
-      />
-      <DynamicImage
-        before={{
-          width: 400,
-          height: 400,
-          layout: 'constrained',
-          src: providers[selectedProvider].src,
-        }}
-        after={{
-          width: 300,
-          height: 200,
-          layout: 'fullWidth',
-          src: providers[selectedProvider].src,
-        }}
       />
     </>
   );
